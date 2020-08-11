d7dd8aa77be164c468308987962ea12bb6e17a69
marcio.v.ordonez@gmail.com <marcio.v.ordonez@gmail.com>
2020-08-11 18:01:30 -0300

    few updates

--- src/controllers/AuthenticationController.ts
+++ src/controllers/AuthenticationController.ts
@@ -10,7 +10,6 @@ export default new class AuthenticationController{
     async create(req: Request, res: Response){
         const {name, email, password} = req.body;
         const [user] = await db('users').where('email','=',email);
-        console.log(user);
         if (!user){
             await db('users').insert({
                 name,
@@ -20,5 +19,18 @@ export default new class AuthenticationController{
             return res.status(200).json({message:"user created successfully!"});
         }
         return res.status(400).json({message:"User already exists"});
+    }
+
+    async logIn(req: Request, res: Response){
+        const {email,password} = req.body;
+        const [user] = await db('users').where('email','=',email);
+
+        if (!user) return res.status(400).json({message:"Email not found"});
+
+        if (!authentication.comparePassword(password,user.password)) return res.status(400)
+        .json({message:"Password does not match!"});
+
+        // you'r logged in
+
     }
 }
--- src/utils/Authentication.ts
+++ src/utils/Authentication.ts
@@ -4,4 +4,8 @@ export default class Authentication{
     async hashPassword(password: string){
         return await bcrypt.hash(password,10);
     }
+
+    async comparePassword(hash: string, password: string){
+        return await bcrypt.compare(password,hash);
+    }
 }
